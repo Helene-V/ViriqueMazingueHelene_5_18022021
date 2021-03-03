3aa97c613db54eb7de450bc557e4f03c274e5c7b
Ln <rainbowliketravel@gmail.com>
2021-03-03 18:05:07 +0100

    affichage des produits et création de balises

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-fetch('http://localhost:3000/api/teddies/')
-.then((resp) => resp.json()) 
-.then(function(data) {
-    let teddie = data
-    return teddie.map(function(teddie){
-    let div = document.createElement('div')
-    let a = document.createElement('a')
-    a.setAttribute('href', 'product.html', '') 
+fetch('http://localhost:3000/api/teddies')
+.then((resp) => resp.json())                 // promesse 1
+.then(function(data) {                       // promesse 2
+    let teddie = data                        // import des teddies avec la data
+    return teddie.map(function(teddie) {     // fonction pour l'affichage des teddies et infos avec return
+    
+    let titreTeddie = document.createElement('div')
+    let article = document.createElement('a')
+    let description = document.createElement('p')
+    let photo = document.createElement('img')
+    let prix = document.createElement('div')
+    
+    article.setAttribute('href', 'product.html',)
+    photo.setAttribute('src', teddie.imageUrl)
+    photo.setAttribute('alt','Photo de Teddie made in France')
+    
+    let descriptionContaint = document.createTextNode(teddie.description)
     let divContaint = document.createTextNode(teddie.name)
-    div.appendChild(divContaint)
-    a.appendChild(div)
-    let elt = document.getElementById("galerie");
-    elt.appendChild(a)
-    console.log(teddie)
+    let prixContaint = document.createTextNode(teddie.price)
+    prix.innerHTML = '€ '    // trouver comment placer '€' après les chiffres
+    
+    titreTeddie.appendChild(divContaint)
+    article.appendChild(titreTeddie)
+    description.appendChild(descriptionContaint)  
+    article.appendChild(description)
+    prix.appendChild(prixContaint)    //bcp de lignes mais ça fonctionne
+    article.appendChild(prix)
+    article.appendChild(photo)
+    
+    let containerGalerie = document.getElementById("galerie");
+    containerGalerie.appendChild(article)
+    console.log(containerGalerie)
+   
     })
 })
-let teddies = document.querySelector('#galerie');
+const afficherGalerie = document.querySelectorAll('#galerie');
+
+
+// window : représente la fenêtre du navigateur
+// document : page html
+// innerHTML : intégration de données
+// page produit avec travail de FOREACH
+// promesses et callback hell
+// faire fonctionner la page index et produit et comment faire le panier
