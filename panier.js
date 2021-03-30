1308d5d4b3d06333cdc15d57138383da9d0cf8fc
Ln <rainbowliketravel@gmail.com>
2021-03-30 09:50:35 +0200

    ajout formulaire page panier

--- panier.js
+++ panier.js
@@ -22,99 +22,101 @@ getPrez.innerHTML = `<h1 class="mt-5 pt-5 pb-4 d-flex justify-content-center">Co
 //getArticle.innerHTML = `<div>${teddie.name}</div>`
 
 let getForm = document.getElementById('information')
-getForm.innerHTML = `<form>
-<div class="form-row d-flex justify-content-center">
-<div class="form-group col-md-2">
-  <label for="inputFirstname">First name</label>
-  <input type="text" class="form-control" id="inputFirstname" placeholder="ex. Albert">
-</div>
-<div class="form-group col-md-2">
-  <label for="inputLastName">Last name</label>
-  <input type="text" class="form-control" id="inputLastname" placeholder="ex. Dupont">
-</div>
-<div class="form-group col-md-2">
-<label for="inputAdress">Adress</label>
-<input type="text" class="form-control" id="inputAdress" placeholder="ex. 2, rue ma campagne">
-</div>
-</div>
-<div class="form-row d-flex justify-content-center">
-  <div class="form-group col-md-3">
-    <label for="inputCity">City</label>
-    <input type="text" class="form-control" id="inputCity" placeholder="ex. Lille">
-  </div>
-  <div class="form-group col-md-3">
-    <label for="inputEmail4">Email</label>
-    <input type="email" class="form-control" id="inputEmail4" placeholder="ex. adupont@gmail.com">
-  </div>
-  </div>
-  <div class="form-row d-flex justify-content-center">
-<button type="submit" class="btn btn-primary">Valider ma commande</button>
-</div>
-</form>`
+// getForm.innerHTML = `<form></form>`
 
 // RECUPERATION DES DONNEES
-let firstName = document.getElementById('information')
-let contact = {
-  firstName: firstName.value,
-  lastName: lastName.value,
-  adress: adress.value,
-  city: city.value,
-  email: email.value
+let firstName = document.getElementById('inputFirstName'),
+  lastName = document.getElementById('inputLastName'),
+  address = document.getElementById('inputAddress'),
+  city = document.getElementById('inputCity'),
+  email = document.getElementById('inputEmail')
+
+  // REGEX VERIFICATION VALUE INPUT
+
+  function testFormulaire(form) {
+  valid = validationFirstName(form.firstName.value)
+  valid += validationLastName(form.lastName.value)
+  valid += validationAddress(form.address.value)
+  valid += validationCity(form.city.value)
+  valid += validationEmail(form.email.value)
+
+  if (valid == "")
+    validation = input.pushValidation()
+  else {alert(test)
+    return false
+  }
 }
-console.log(contact)
+//let maCommande = JSON.parse(sessionStorage.getItem('commande'))
 
-let products = [
-// push les id dans products avec une boucle for of
-]
-console.log(products)
+  // CLICK VALIDATION FORMULAIRE
+let validation = document.getElementById('validation')
+validation.addEventListener('click', function() {
+  let contact = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    address: address.value,
+    city: city.value,
+    email: email.value
+  }
+  let products = [
+    '5be9c8541c9d440000665243', '5beaa8bf1c9d440000a57d94'
+    // push les id dans products avec une boucle for of
+    ]
+    let postData = {
+      contact, products
+    }
+    let jsonPostData = JSON.stringify('postData')
+    console.log(postData)
+    console.log(jsonPostData)   
+    fetch('http://localhost:3000/api/teddies/' + 'order', {
+    method: 'POST',
+    mode : 'cors',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    },
+    body: jsonPostData,
+})
+.then(response => response.json()) 
+.then(json => console.log(json))
+.catch(err => console.log(err))
 
+ //sessionStorage.setItem('maCommande', response.orderId)
+  //window.location.href = 'confirmation.html'
+  })
 
 /*
-document.getElementById('inputEmail4').addEventListener('click',function(){
+document.getElementById('validation').addEventListener('click',function(){
   let valide = true
-  for(let input of document.document.getElementById('information')){
+  for(let input of document.getElementById('div')){
     valid = input.pushValidation()
     if(valide){
       break
     }
   }
   if(valide){
-    // idee : POST, les PDTS EN window location pour ouvrir la page de confirmation
-    /* info : faire une page de confirmation de commande, remerciant l'utilisateur pour sa
-    commande, et indiquant le prix total et l'identifiant de commande envoyé
-    par le serveur. order_id
-*/
 
-/*
-function totalPrice(teddie) {
-  for (let i = 0; i< teddie.length; i++) {
-    let teddie = teddie[i]
-}
-  console.log(totalPrice)
-*/
+  }*/
+  
+  // info : faire une page de confirmation de commande, remerciant l'utilisateur pour sa
+  //commande, et indiquant le prix total et l'identifiant de commande envoyé
+  //par le serveur. order_id
 
-/*
-let getElt = product
-this.product = product.id
-this.product = product.name + product.price
-*/
+
+//function totalPrice(teddie) {
+//  for (let i = 0; i< teddie.length; i++) {
+//    let teddie = teddie[i]
+//}
+//  console.log(totalPrice)
+
+
+//let getElt = product
+//this.product = product.id
+//this.product = product.name + product.price
+
 
 //sessionStorage.getItem('price') = document.getElementById('affichage').innerHTML
 //sessionStorage.getItem('name') = document.getElementById('affichage').innerHTML
 
+
 //let nom = sessionStorage.getItem('name').innerHTML
 //let prix = sessionStorage.getItem('price').innerHTML
-
-
-// faire un objet global qui regroupe contact + products, l'envoyer en post via un nouveau sessionStorage ex bonDeCommande sur la page commande
-
-// On peut construire un préfixe HTTP
-// afin de construire plus facilement
-// des requêtes via des substitutions
-/*
-POST`http://toto.org/truc?a=${a}&b=${b}
-     Content-Type: application/json
-     X-Credentials: ${credentials}
-     { "toto": ${toto},
-       "truc": ${truc}}`(myOnReadyStateChangeHandler);
-*/
